b2bff6d678cabef41cb2800d2118a774c4949745
Archon <den_archangel@mail.ru>
2020-06-18 08:26:32 +0500

    Updates

---
+++ precache-manifest.ff65f5b3bf12cfa2bc89d18170c7875c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7327515590174f0333de7c9aeba204a9",
+    "url": "/demo_aviasales/index.html"
+  },
+  {
+    "revision": "21660be0705d0dcb3a69",
+    "url": "/demo_aviasales/static/css/main.bf93bc3e.chunk.css"
+  },
+  {
+    "revision": "15fe2ccf0a307537e48f",
+    "url": "/demo_aviasales/static/js/2.5ef1ca36.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demo_aviasales/static/js/2.5ef1ca36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21660be0705d0dcb3a69",
+    "url": "/demo_aviasales/static/js/main.0db37cad.chunk.js"
+  },
+  {
+    "revision": "e588e78a06b734f16795",
+    "url": "/demo_aviasales/static/js/runtime-main.a8e19250.js"
+  },
+  {
+    "revision": "0c08923414ada761c4d56d4927f4d1d9",
+    "url": "/demo_aviasales/static/media/logo.0c089234.svg"
+  },
+  {
+    "revision": "c8696f38f823e82d8c9142839d0e5d5f",
+    "url": "/demo_aviasales/static/media/open-sans-600.c8696f38.woff2"
+  },
+  {
+    "revision": "aeb4b1bfc96bce2be7d95d9917d8cccb",
+    "url": "/demo_aviasales/static/media/open-sans-regular.aeb4b1bf.woff2"
+  }
+]);
